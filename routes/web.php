4de9b49671c33ba01dbd0c50442109153351226d
Alpha2Chris14 <onyekachristian14@gmail.com>
2024-11-05 10:59:05 +0100

    modified the readme and autoload in the composer.json

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,30 @@ Route::get('/', function () {
 
 Route::get('/test-routing', function () {
     $router = new PaymentRouter();
-    $router->addProcessor('processor1', ['fee_percentage' => 0.01]);
-    $router->addProcessor('processor2', ['fee_percentage' => 0.02]);
+    $router->addProcessor('processor1', [
+        'fee_percentage' => 0.01,
+        'active' => true,
+        'success_rate' => 0.98,
+        'average_response_time' => 400,
+        'supported_countries' => ['US'],
+        'supported_currencies' => ['USD']
+    ]);
+    $router->addProcessor('processor2', [
+        'fee_percentage' => 0.02,
+        'active' => false,
+        'success_rate' => 0.96,
+        'average_response_time' => 300,
+        'supported_countries' => ['NG'],
+        'supported_currencies' => ['NGN']
+    ]);
 
-    $transaction = ['amount' => 100];
+
+    $transaction = [
+        'amount' => 100,
+        'country' => 'US',
+        'currency' => 'USD'
+    ];
+
     $bestProcessor = $router->route($transaction);
 
     return response()->json(['best_processor' => $bestProcessor]);
